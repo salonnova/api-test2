6c81ac3bbc1002e6ebc2bea1cfcf66923d15d9a4
novasalmon <novanekuro@gmail.com>
2023-10-06 21:58:05 +0900

    eighth

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\user_create;
 
+
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
